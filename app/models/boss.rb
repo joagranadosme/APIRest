8ea45c47ebfe8c26f42d387953b4a17cfdc8b705
Jonathan Granados <joagranadosme@unal.edu.co>
2017-09-02 03:55:14 +0000

    validates

--- app/models/boss.rb
+++ app/models/boss.rb
@@ -1,2 +1,9 @@
 class Boss < ApplicationRecord
+    
+    validates :name, presence: true
+    validates :lastname, presence: true
+    validates :charge, presence: true
+    validates :salary, presence: true
+    
+    
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,2 +1,9 @@
 class Pet < ApplicationRecord
+    
+    validates :name, presence: true
+    validates :race, presence: true
+    validates :age, presence: true
+    validates :width, presence: true
+    validates :height, presence: true
+    
 end
